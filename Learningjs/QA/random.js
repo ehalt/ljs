bd108b7f2744fb252429c54980f13c3b5c68eb45
ehalt <torikussadikk@gmail.com>
2020-11-06 22:45:54 -0800

    next

--- Learningjs/QA/random.js
+++ Learningjs/QA/random.js
@@ -1,4 +1,13 @@
-var num = 2.523423;
-var result = Math.floor(num);
-var result2 = Math.ceil(num);
-console.log(result2);
+
+var a = {
+    name:'abcd',
+    age: 19
+}
+
+var v = JSON.stringify(a);
+console.log(v);
+
+var ob = JSON.parse(v);
+ob;
+var i = ob.name;
+i;
